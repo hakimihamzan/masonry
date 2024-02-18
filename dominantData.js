7a74de3eb0ec4824be100f003a31d475f5d04d42
hakimihamzan <hakimihamzan20@gmail.com>
2024-02-18 21:20:08 +0800

    - setup files - install masonry, jquery - able to show images using masonry

---
+++ dominantData.js
@@ -0,0 +1,52 @@
+let data = [
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/1.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/2.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/3.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/4.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/5.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/6.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/7.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/8.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/9.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/10.jpg',
+    },
+];
