0b2e6dacbc38bd5b547b363472f1b3bfcf12a8f3
hakimihamzan <hakimihamzan20@gmail.com>
2024-02-18 23:12:20 +0800

    add loadMore button to add images dynamically

--- dominantData.js
+++ dominantData.js
@@ -49,4 +49,54 @@ let data = [
         person: ['kimi', 'ley'],
         path: '/dominant_img/10.jpg',
     },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/11.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/12.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/13.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/14.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/15.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/16.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/17.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/18.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/19.jpg',
+    },
+    {
+        name: 'temp name',
+        person: ['kimi', 'ley'],
+        path: '/dominant_img/20.jpg',
+    },
 ];
